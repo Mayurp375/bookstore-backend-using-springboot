8b9017b49baded81ac7cb155f9b5a94becc4116c
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-10-01 17:56:50 +0530

    chenge in book

--- src/main/java/bookstore/bookstore/entity/Book.java
+++ src/main/java/bookstore/bookstore/entity/Book.java
@@ -13,19 +13,17 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Book {
 
-//    category:"Non-Fiction"
-//    description:"200"
-//    id:1
-//    image:"https://m.media-amazon.com/images/I/814L+vq01mL._SY466_.jpg"
-//    name:"Ikigai Hardcover"
+//"name":"psychology of money",
+//        "price":"100",
+//        "category":"Fiction",
+//        "image":"https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL200_SR200,200_.jpg"
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String category;
     private Double price;
-
+    private String category;
     private String image;
 
     // ... getters, setters, constructors ...
